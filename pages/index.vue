7a615e4dbafe0e362520c477a1ace4cf3b5f068f
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-23 13:56:23 +0400

    Add index.vue

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="page">
+    <h1>Basic Calculator</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.page { display:flex; flex-direction:column; align-items:center; padding:2rem; }
+</style>
