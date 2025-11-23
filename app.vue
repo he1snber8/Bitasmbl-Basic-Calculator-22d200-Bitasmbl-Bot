d08112c981afa03ceaa5c05ccd2f70018b9e476c
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-23 13:56:24 +0400

    Add app.vue

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtPage />
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+html, body, #__nuxt { height: 100%; margin: 0; }
+body { font-family: system-ui, sans-serif; }
+</style>
